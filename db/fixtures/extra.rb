9ea14efc8075d6812ff69084dcff2f21c64bc66c
Yuichi Ajiro <ajiro99@gmail.com>
2019-02-14 11:33:31 +0900

    add seed_fu

---
+++ db/fixtures/extra.rb
@@ -0,0 +1,34 @@
+Extra.seed do |s|
+  s.id = 1
+  s.name = 'エアブラシ'
+  s.price = 108
+  s.display = true
+end
+
+Extra.seed do |s|
+  s.id = 2
+  s.name = 'マイクロファイバークロス'
+  s.price = 108
+  s.display = true
+end
+
+Extra.seed do |s|
+  s.id = 3
+  s.name = 'ミニ三脚'
+  s.price = 108
+  s.display = true
+end
+
+Extra.seed do |s|
+  s.id = 4
+  s.name = 'WiFi SD(8GB)(1,880)'
+  s.price = 1880
+  s.display = true
+end
+
+Extra.seed do |s|
+  s.id = 5
+  s.name = 'WiFi SD(4GB)(2,000)'
+  s.price = 2000
+  s.display = true
+end
---
+++ db/fixtures/product.rb
@@ -0,0 +1,97 @@
+Product.seed do |s|
+  s.id = 1
+  s.type = 'Body'
+  s.maker = 1
+  s.name = 'E-PL1'
+end
+
+Product.seed do |s|
+  s.id = 2
+  s.type = 'Body'
+  s.maker = 1
+  s.name = 'E-PL1s'
+end
+
+Product.seed do |s|
+  s.id = 3
+  s.type = 'Body'
+  s.maker = 1
+  s.name = 'E-PL2'
+end
+
+Product.seed do |s|
+  s.id = 4
+  s.type = 'Body'
+  s.maker = 1
+  s.name = 'E-PL3'
+end
+
+Product.seed do |s|
+  s.id = 5
+  s.type = 'Body'
+  s.maker = 1
+  s.name = 'E-PL5'
+end
+
+Product.seed do |s|
+  s.id = 6
+  s.type = 'Body'
+  s.maker = 1
+  s.name = 'E-PL6'
+end
+
+Product.seed do |s|
+  s.id = 7
+  s.type = 'Body'
+  s.maker = 1
+  s.name = 'E-PL7'
+end
+
+Product.seed do |s|
+  s.id = 8
+  s.type = 'Body'
+  s.maker = 1
+  s.name = 'E-PL8'
+end
+
+Product.seed do |s|
+  s.id = 9
+  s.type = 'Body'
+  s.maker = 1
+  s.name = 'E-PL9'
+end
+
+Product.seed do |s|
+  s.id = 10
+  s.type = 'Lense'
+  s.maker = 1
+  s.name = 'M.ZUIKO 14-42mm'
+end
+
+Product.seed do |s|
+  s.id = 11
+  s.type = 'Lense'
+  s.maker = 1
+  s.name = 'M.ZUIKO 40-150mm'
+end
+
+Product.seed do |s|
+  s.id = 12
+  s.type = 'Lense'
+  s.maker = 1
+  s.name = 'M.ZUIKO 17mm'
+end
+
+Product.seed do |s|
+  s.id = 13
+  s.type = 'Lense'
+  s.maker = 1
+  s.name = 'ボディキャップレンズ 15mm'
+end
+
+Product.seed do |s|
+  s.id = 14
+  s.type = 'Lense'
+  s.maker = 2
+  s.name = 'NIKKOR 18-55mm VR'
+end
