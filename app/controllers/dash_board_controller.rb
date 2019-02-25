c6a6a7e5bcf65d7ab52f9140c7c8363b2213749f
Yuichi Ajiro <ajiro99@gmail.com>
2019-02-25 14:47:37 +0900

    add summary in dashboard

--- app/controllers/dash_board_controller.rb
+++ app/controllers/dash_board_controller.rb
@@ -15,10 +15,33 @@ class DashBoardController < ApplicationController
                                          )
 
     @sale_of_by_year = Sale.group('strftime("%Y", sales_date)')
-                            .select(
-                              j[:selling_price].sum.as('selling_price'),
-                              j[:profit].sum.as('profit'),
-                              j[:sales_date]
-                            )
+                           .select(
+                             j[:selling_price].sum.as('selling_price'),
+                             j[:profit].sum.as('profit'),
+                             j[:sales_date]
+                           )
+
+    sales = Sale.where('sales_date >= ? ', '2019-01-01 00:00:00')
+    @week_count = [0, 0, 0, 0, 0, 0, 0]
+    @day_count = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+    sales.map do |sale|
+      @week_count[sale.sales_date.wday] += 1
+      @day_count[sale.sales_date.day - 1] += 1
+    end
+
+    @week_proportion = []
+    @week_count.map do |count|
+      @week_proportion << ActionController::Base.helpers.number_to_percentage(
+        count.fdiv(sales.size) * 100, precision: 0
+      )
+    end
+
+    @day_proportion = []
+    @day_count.map do |count|
+      @day_proportion << ActionController::Base.helpers.number_to_percentage(
+        count.fdiv(sales.size) * 100, precision: 0
+      )
+    end
   end
 end
