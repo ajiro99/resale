5bafc0f029baf4f32628e9e08724f67d63edd5de
Yuichi Ajiro <ajiro99@gmail.com>
2019-03-29 10:39:40 +0900

    fix select extra

--- app/models/extra.rb
+++ app/models/extra.rb
@@ -1,4 +1,5 @@
 class Extra < ApplicationRecord
   has_many :sale_extras
-  scope :display, -> { where(display: true) }
+  scope :display, -> { where(display: true).order(:name) }
+  scope :no_display, -> { where(display: false).order(:name) }
 end
